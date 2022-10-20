d6947d8fdb24ff8a50ccedf4c074a7523de1521c
AntonGatalski <ag629331@gmail.com>
2022-10-20 21:35:58 +0300

    added table

--- src/StackTest.java
+++ src/StackTest.java
@@ -1,7 +1,10 @@
 public class StackTest {
     public static void main(String[] args) {
-      //  Double[] result = Task5.swap(0, 1, 1.5, 2, 3);Incompatible types.
-        Double[] result = Task5.<Double>swap(0, 1, 1.5, 2.0, 3.0);
-
+    Table<Integer, String > t1 = new Table<>();
+t1.put(1,"a");
+        t1.put(2,"b");
+        t1.put(3,"c");
+        t1.put(4,"d");
+        System.out.println(t1.getHash("c"));
     }
 }
--- src/Table.java
+++ src/Table.java
@@ -1,7 +1,32 @@
+import java.util.ArrayList;
+
 public class Table<K, V> {
+
+        Table()
+        {
+            table = new ArrayList<>(1);
+        }
+    public void put(K key, V value)
+    {
+        int bucket = getHash(value);
+        table.add(bucket, new Entry<K,V>(key,value));
+    }
+
+    public int getHash(V value)
+    {
+        return value.hashCode() %  (table.size() + 1);
+    }
+
+    private ArrayList<Entry<K,V>> table;
 
 }
 
 class Entry<K, V> {
-
+    Entry(K key, V value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+    private K key;
+    private V value;
 }
