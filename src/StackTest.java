f14316711de9f1bb8bbc66b9e17aac665a44f8e2
AntonGatalski <ag629331@gmail.com>
2022-10-21 17:03:06 +0300

    added hashtable

--- src/StackTest.java
+++ src/StackTest.java
@@ -1,10 +1,22 @@
 public class StackTest {
     public static void main(String[] args) {
     Table<Integer, String > t1 = new Table<>();
-t1.put(1,"a");
-        t1.put(2,"b");
-        t1.put(3,"c");
-        t1.put(4,"d");
-        System.out.println(t1.getHash("c"));
+        t1.add(1,"a");
+        t1.add(2,"b");
+        t1.add(3,"c");
+        t1.add(4,"d");
+        t1.add(5,"e");
+        t1.add(6,"f");
+        t1.add(7,"g");
+        t1.add(85,"hAD");
+        System.out.println(t1.get(1));
+        System.out.println(t1.get(2));
+        System.out.println(t1.get(3));
+        System.out.println(t1.get(4));
+        System.out.println(t1.get(5));
+        System.out.println(t1.get(6));
+        System.out.println(t1.get(7));
+        System.out.println(t1.get(8));
+
     }
 }
--- src/Table.java
+++ src/Table.java
@@ -2,31 +2,91 @@ import java.util.ArrayList;
 
 public class Table<K, V> {
 
-        Table()
+    class Entry {
+        Entry(K key, V value)
         {
-            table = new ArrayList<>(1);
+            this.key = key;
+            this.value = value;
+            this.next = null;
         }
-    public void put(K key, V value)
-    {
-        int bucket = getHash(value);
-        table.add(bucket, new Entry<K,V>(key,value));
+
+        public Entry next;
+        public K key;
+        public V value;
     }
 
-    public int getHash(V value)
+    Table()
     {
-        return value.hashCode() %  (table.size() + 1);
+        table = new ArrayList<>();
+        size = 0;
+        maxSize = 20;
+
+        for (int i = 0; i < maxSize; ++i)
+        {
+            table.add(null);
+        }
+
+    }
+    public void add(K key, V value)
+    {
+        int index = getHash(key);
+
+        Entry head = table.get(index);
+        while (head != null)
+        {
+            if (head.key.equals(key))
+                head.value = value;
+            head = head.next;
+        }
+
+        ++size;
+        head = table.get(index);
+        Entry newHead = new Entry(key,value);
+        newHead.next = head;
+        table.set(index,newHead);
+        tryIncreaseSize();
     }
 
-    private ArrayList<Entry<K,V>> table;
+    public V get(K key)
+    {
+        int index = getHash(key);
+        Entry head = table.get(index);
+        while (head  != null)
+        {
+            if (head.key.equals(key))
+                return head.value;
+            head = head.next;
+        }
+        return null;
+    }
+
+    public int getHash(K key)
+    {
+        return Math.abs(key.hashCode()) %  table.size();
+    }
+
+    private void tryIncreaseSize()
+    {
+        if ((double)size/(double)maxSize >= 0.7) {
+            ArrayList<Entry> tempTable = new ArrayList<>();
+            maxSize += 10;
+            size = 0;
+            for (int i = 0; i < maxSize; ++i) {
+                table.add(null);
+                for (Entry headEntry : tempTable)
+                {
+                    while (headEntry != null)
+                    {
+                        add(headEntry.key,headEntry.value);
+                        headEntry = headEntry.next;
+                    }
+                }
+            }
+        }
+    }
+    private ArrayList<Entry> table;
+    private int size;
+    private int maxSize;
 
 }
 
-class Entry<K, V> {
-    Entry(K key, V value)
-    {
-        this.key = key;
-        this.value = value;
-    }
-    private K key;
-    private V value;
-}
